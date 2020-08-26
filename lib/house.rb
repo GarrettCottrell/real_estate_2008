c4d5baa0cd37174f3def661e8d15b66f5204cb47
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-08-26 10:56:48 -0600

    created house class and tests - couldn't figure out what to put in assert equal for details but got the method giving the right output in pry. Couldn't figure out the category method

--- lib/house.rb
+++ lib/house.rb
@@ -1,14 +1,39 @@
 require './lib/room'
 
 class House
-attr_reader :price, :address, :rooms
-def initialize(price, address)
-  @price = price
+attr_reader :address, :rooms, :cost
+def initialize(cost, address)
   @address = address
   @rooms = []
+  @cost = cost
   end
 
+  def price
+    price_hash = Hash.new(0)
+    price_hash[@cost] = 400000
+    price_hash[@cost]
+end
   def add_room(room)
     @rooms << room
+  end
+  def above_market_value
+    if price > 500000
+      true
+    else
+      false
+    end
+  end
+  def rooms_from_category(room)
+   @rooms.find_all do |rooms|
+    rooms == room
+    end
+  end
+
+  def details
+
+    details_hash = Hash.new
+    details_hash["price"] = price
+    details_hash["address"] = address
+    details_hash
   end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -10,7 +10,7 @@ class HouseTest < Minitest::Test
   end
   def test_price
     house = House.new("$400000", "123 sugar lane")
-    assert_equal "$400000", house.price
+    assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
     assert_equal [], house.rooms
   end
@@ -24,6 +24,31 @@ class HouseTest < Minitest::Test
     house.add_room(room_2)
 
     assert_equal [room_1, room_2], house.rooms
-    require "pry";binding.pry
+
+  end
+  def test_above_market_value
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_value
+  end
+
+  # def test_rooms_from_category
+  #   house = House.new("$400000", "123 sugar lane")
+  #   room_1 = Room.new(:bedroom, 10, '13')
+  #   room_2 = Room.new(:bedroom, 11, '15')
+  #   room_3 = Room.new(:living_room, 25, '15')
+  #   room_4 = Room.new(:basement, 30, '41')
+  #   house.add_room(room_1)
+  #   house.add_room(room_2)
+  #   house.add_room(room_3)
+  #   house.add_room(room_4)
+  #   require "pry";binding.pry
+  #   assert_equal [room_1.category, room_2.category], house.rooms_from_category(:bedroom)
+  # end
+
+  def test_house_details
+  house = House.new("$40000", "123 sugar lane")
+require "pry";binding.pry
+  assert_equal {"price"==>400000, "address"==>"123 sugar lane"}, house.details
   end
 end
