1d7687b214afab171764f40339a47414e116cd43
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-08-26 10:06:22 -0600

    Created house and room classes w/ tests. All tests passing - couldn't figure out how to change  to an integer without $

---
+++ test/house_test.rb
@@ -0,0 +1,29 @@
+require './lib/room'
+require './lib/house'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class HouseTest < Minitest::Test
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+  def test_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms
+  end
+
+  def test_add_room
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
+    require "pry";binding.pry
+  end
+end
